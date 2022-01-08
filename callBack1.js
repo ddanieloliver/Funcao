8bcefc055fed1009a962943ce05b35bde434cc09
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-08 15:02:51 -0300

    Aprendendo o conceito de callback

---
+++ callBack1.js
@@ -0,0 +1,10 @@
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
+
+//callback significa que vou passar uma função e essa função vai voltar quando o evento acontecer.
