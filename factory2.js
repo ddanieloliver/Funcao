4520dfa60612afb13f642824afe6655476c6a0fd
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-10 17:08:48 -0300

    Fazendo na pratica uma função fábrica e vendo como finciona

---
+++ factory2.js
@@ -0,0 +1,10 @@
+function criarProduto(nome, preco) {
+   return{
+    nome,
+    preco,
+    desconto: 0.1}
+}
+console.log(criarProduto('IphoneX', preco = 4499.49))
+console.log(criarProduto('Tablet', preco = 1499.49))
+console.log(criarProduto('Galaxy s10', preco = 3499.59))
+console.log(criarProduto('Iphone12', preco = 6499.49))
