c50268bf53cc15730e3594bb147a99b1c71f7cb0
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-10 14:22:51 -0300

    Vendo como posso amarrar o this a um objeto Como resolver o problema do this a partir de multiplas chamadas

--- thisEBind1.js
+++ thisEBind1.js
@@ -11,4 +11,3 @@ falar() // conflito entre paradigmas: funcional e OO
 
 const falarDePessoa = pessoa.falar.bind(pessoa) // isso sgnifica que eu amarrei o objeto ao this, enão sempre que eu chamar o this, ele fará referencia ao objeto amarrado
 falarDePessoa()
- 
