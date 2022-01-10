1a4bef9ad67c2c32f18d592119320d073abf77b1
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-10 18:40:14 -0300

    aprendendo o que é IIEF e para que serve

---
+++ IIFE.js
@@ -0,0 +1,7 @@
+//IIFE ->   Immediately Invoked Function Expression
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
+//Isso é muito utilizado para fugir do escopo global
