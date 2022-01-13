cabd302bb7a9da82d5588d5f51c63cb0be61d3b9
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-13 16:54:28 -0300

    Achei fácil

---
+++ atividade4.js
@@ -0,0 +1,7 @@
+//Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+
+function exponencial(base, expoente){
+   let resultado = base ** expoente
+return resultado}
+
+console.log(exponencial(2,3))
